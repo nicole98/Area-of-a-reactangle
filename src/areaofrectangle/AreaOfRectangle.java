8f1501e7193ab93d6cf6cdeee4266ebffd6647be
nitao5001 <nitao5001@7KVM6Y1.Student.UGDSB.ED>
2018-02-28 13:55:06 -0500

    Area of a rectangle

---
+++ src/areaofrectangle/AreaOfRectangle.java
@@ -0,0 +1,30 @@
+/*
+ * Nicole Tao
+ * AreaOfRectangle.java
+ * This program calculates the area of a rectangle.
+ * February 27th, 2018
+ */
+
+package areaofrectangle;
+
+/**
+ *
+ * @author nitao5001
+ */
+public class AreaOfRectangle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double width = 5.7;
+        double length = 4.8;
+        //This will calculate the area of the rectangle
+        double area = width*length;
+        
+        System.out.println("the width is " + width);
+        System.out.println("the length is " + length);
+        System.out.println("the area is " + area);
+    }
+    
+}
